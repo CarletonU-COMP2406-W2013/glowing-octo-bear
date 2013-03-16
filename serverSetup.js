e2006c416be21f2e15a59a8762d9fec4186310fb
bfairclo <benfairclough@cmail.carleton.ca>
2013-03-15 21:46:12 -0400

    Better draggable UX

--- serverSetup.js
+++ serverSetup.js
@@ -1,1 +1,44 @@
-mongo OrderUp --eval "printjson(db.getCollectionNames())";
+// start your instance of mongodb - $ mongod
+// then run - $ mongo localhost:27017/OrderUp serverSetup.js
+
+// BEVERAGES
+db.menu.save({
+	_id: 100,
+	name: "Root Beer",
+	price: "$2.99",
+	type: "Beverage"
+});
+db.menu.save({
+	_id: 101,
+	name: "Real Beer",
+	price: "$5.99",
+	type: "Beverage"
+});
+
+// MAIN COURSES
+db.menu.save({
+	_id: 200,
+	name: "Double Bacon Whiskey BBQ Burger",
+	price: "$12.99",
+	type: "Food"
+});
+db.menu.save({
+	_id: 201,
+	name: "Chicken Parmesan",
+	price: "$15.99",
+	type: "Food"
+});
+
+// DESSERTS
+db.menu.save({
+	_id: 300,
+	name: "Triple Chocolate Cheesecake ",
+	price: "$6.99",
+	type: "Dessert"
+});
+db.menu.save({
+	_id: 301,
+	name: "Pumpkin Pie",
+	price: "$5.99",
+	type: "Dessert"
+});
